bfd00f2e98c409310acb46df6509cfae98c5751c
Jothamyacim <134441736+Jothamyacim@users.noreply.github.com>
2025-12-09 12:09:09 +0100

    completed grid-squares container

--- Foundations_Course/03-Etch-a-Sketch/script.js
+++ Foundations_Course/03-Etch-a-Sketch/script.js
@@ -5,3 +5,22 @@ console.log("Hello world!");
 document.getElementById("#container");
 
 //define grid rules
+const rows = 16;
+const columns = 16;
+
+const totalSquares = rows*columns;//16 * 16 = 256 squares
+
+//loop to genenrate squares
+for(let i = 0; i < totalSquares; i++) {
+
+    //create div tags for every square
+    const square = document.createElement('div');
+
+    //add classList for styling
+    square.classList.add('grid-square');
+
+    //append squares to container
+    container.appendChild(square);
+
+}
+
